e90d367ba361eb8f1577baf85a953c5a6b069f42
br-Yan1993 <84878538+br-Yan1993@users.noreply.github.com>
2024-05-09 20:03:29 -0300

    adicionado_script_com_funcao

---
+++ banco_js/src/backend/home.js
@@ -0,0 +1,17 @@
+vet_user = [
+    "joao_video",
+    "caue_adm",
+    "gabi_js"
+]
+
+vet_senha = [
+    "12349",
+    "adm3",
+    "987654"
+]
+
+function acessar() {
+    user = document.getElementById('user').value;
+    alert(user);
+    //
+}
